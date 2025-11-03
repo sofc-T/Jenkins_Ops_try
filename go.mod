55e1ca5f4ad43f0316903fce898d91abccd7dd2b
Sof <sofstzz@gmail.com>
2025-11-03 11:12:44 +0300

    Jenkins iti si

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sofc-T/Jenkins_Ops_try
+
+go 1.24.7
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Warning: .env file not found or couldn't be loaded.")
+	}
+
+	val := os.Getenv("WHATSUP_VALUE")
+	if val == "" {
+		//  if no whatsup value is set, use boring old 
+		val = "Hello, World!"
+	}
+
 	http.HandleFunc("/whatsup", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "What's up world!")
+		fmt.Fprintln(w, val)
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
